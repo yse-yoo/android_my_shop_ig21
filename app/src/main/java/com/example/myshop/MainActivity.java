496b3f1c71875047f34c5e86afe3cccae4e625fc
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-14 14:04:56 +0900

    update app

--- app/src/main/java/com/example/myshop/MainActivity.java
+++ app/src/main/java/com/example/myshop/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.myshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String INFO = "info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(INFO, "onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(INFO, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(INFO, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(INFO, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(INFO, "onStop");
+    }
+
+
 }
